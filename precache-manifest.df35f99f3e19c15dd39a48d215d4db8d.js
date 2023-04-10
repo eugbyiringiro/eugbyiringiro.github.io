6de909e5d448186105861f44cd43cd37fda32560
Pantheraleo1007 <130156914+eugbyiringiro@users.noreply.github.com>
2023-04-10 18:19:40 +0200

    Updates

---
+++ precache-manifest.df35f99f3e19c15dd39a48d215d4db8d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6683bfff94d66ceba449efeb8a948443",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9cce5f344a53952becc",
+    "url": "/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "e35ecca10de4933f37c2",
+    "url": "/static/css/main.5f74b5a3.chunk.css"
+  },
+  {
+    "revision": "f9cce5f344a53952becc",
+    "url": "/static/js/2.5c64cfbc.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.5c64cfbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e35ecca10de4933f37c2",
+    "url": "/static/js/main.7179e950.chunk.js"
+  },
+  {
+    "revision": "3a172e9daf29f6cc3071",
+    "url": "/static/js/runtime-main.08f98dcf.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/eugbyiringiro.github.io/precache-manifest.fb3eb2b565a834649602a285224f6a40.js"
+  "/precache-manifest.df35f99f3e19c15dd39a48d215d4db8d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/eugbyiringiro.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
